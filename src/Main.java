f2c7108c9f06430e9045df7407ca8a6b8b7e0b2e
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-02 14:14:56 +0900

    third if-else

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        int number = -1;
+        int number = 0;
 
-        if (number >= 0){
-            System.out.println("number는 0보다 큽니다.");
+        if (number > 0) {
+            System.out.println("number는 양수입니다.");
+        } else if (number <0) {
+            System.out.println("number는 음수입니다.");
+        } else {
+            System.out.println("number는 0입니다.");
         }
-
-        else {
-            System.out.println("number는 0이거나 0보다 작습니다.");
-        }
-
     }
 }
