fabeb34d45e416483ce5559985054b28b5876eec
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-02 13:31:52 +0900

    second if-else

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        int number = -1;
+
+        if (number >= 0){
+            System.out.println("number는 0보다 큽니다.");
+        }
+
+        else if (number <= 0){
+            System.out.println("number는 0이거나 0보다 작습니다.");
+        }
 
     }
 }
